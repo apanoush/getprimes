528231fb49276f30455138bd0c1c33410fffb6e3
anoush <dakeon@outlook.fr>
2022-10-29 17:08:31 +0200

    1

---
+++ getprimes-alt.scala
@@ -0,0 +1,26 @@
+// alt version that use built-in functions instead of pattern matching
+
+object main {
+
+def main(args: Array[String]) = {
+    
+    def getPrimes (n: Int): List[Int] = {
+
+        def isPrime(k: Int): Boolean = {
+            (2 until k).forall(x => k % x != 0)
+        }
+        
+        def acc(number: Int, list: List[Int]): List[Int] = { number match 
+            case 0 => list
+            case _ => if (isPrime(number)) then acc(number -1, list :+ number) else acc(number -1, list)
+        }
+
+        lazy val acc2: List[Int] = (1 to n).toList.filter(isPrime(_))
+
+        //acc(n, List()).reverse
+        acc2
+    }
+
+    if (args.isEmpty) then print(getPrimes(100)) else print(getPrimes(args(0).toInt))
+    }
+}
